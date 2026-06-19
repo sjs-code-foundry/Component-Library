f8a4965a361a3a0767c1784b55be6e1c28f4bcdd
sjs-code-foundry <104642983+sjs-code-foundry@users.noreply.github.com>
2026-06-19 19:42:23 +0100

    feat: Add context for dark mode
    Test button included; also CSS imports set up.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import "./App.css";
 
-import { useRef } from "react";
+import { createContext, useRef } from "react";
+import useToggle from "./hooks/useToggle";
 
 import Header from "./components/Header/index";
 
+const LightDarkContext = createContext();
+export { LightDarkContext };
+
 function App() {
   document.title = "Component Library";
+
+  const [dark, toggleDark] = useToggle(false, () =>
+    console.log(`Dark Mode: ${dark}`),
+  );
 
   const appRef = useRef(null);
   // const bodyRef = appRef.current.parentNode.parentNode;
@@ -13,40 +21,45 @@ function App() {
   // bodyRef only works when app is initialised
 
   return (
-    <div className="App" ref={appRef}>
-      <Header>
-        <Header.Button onClick={() => console.log("Badges button clicked.")}>
-          Badges
-        </Header.Button>
-        <Header.Button onClick={() => console.log("Banners button clicked.")}>
-          Banners
-        </Header.Button>
-        <Header.Button onClick={() => console.log("Cards button clicked.")}>
-          Cards
-        </Header.Button>
-        <Header.Button
-          onClick={() => console.log("Testimonials button clicked.")}
-        >
-          Testimonials
-        </Header.Button>
-        <Header.Button onClick={() => console.log("Tooltips button clicked.")}>
-          Tooltips
-        </Header.Button>
-        <Header.Button
-          onClick={() => console.log("Toast Popups button clicked.")}
-        >
-          Toast Popups
-        </Header.Button>
-      </Header>
-      <main>
-        <section className="entry-form">
-          <p>This is where the entry form will go.</p>
-        </section>
-        <section className="render-window">
-          <p>This is where the rendering window will go.</p>
-        </section>
-      </main>
-    </div>
+    <LightDarkContext.Provider value={{ dark, toggleDark }}>
+      <button onClick={toggleDark}>Toggle Dark Mode</button>
+      <div className="App" ref={appRef}>
+        <Header>
+          <Header.Button onClick={() => console.log("Badges button clicked.")}>
+            Badges
+          </Header.Button>
+          <Header.Button onClick={() => console.log("Banners button clicked.")}>
+            Banners
+          </Header.Button>
+          <Header.Button onClick={() => console.log("Cards button clicked.")}>
+            Cards
+          </Header.Button>
+          <Header.Button
+            onClick={() => console.log("Testimonials button clicked.")}
+          >
+            Testimonials
+          </Header.Button>
+          <Header.Button
+            onClick={() => console.log("Tooltips button clicked.")}
+          >
+            Tooltips
+          </Header.Button>
+          <Header.Button
+            onClick={() => console.log("Toast Popups button clicked.")}
+          >
+            Toast Popups
+          </Header.Button>
+        </Header>
+        <main>
+          <section className="entry-form">
+            <p>This is where the entry form will go.</p>
+          </section>
+          <section className="render-window">
+            <p>This is where the rendering window will go.</p>
+          </section>
+        </main>
+      </div>
+    </LightDarkContext.Provider>
   );
 }
 
