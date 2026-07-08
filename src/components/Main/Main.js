5ed0204bc7084a4d334b6f2c2526e93cd27d0a4c
sjs-code-foundry <104642983+sjs-code-foundry@users.noreply.github.com>
2026-07-08 19:34:42 +0100

    feat: Switch statement for rendering required elements

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import Form from "../EntryForm";
 import Badge from "../ComponentElements/Badge";
 
@@ -5,6 +7,33 @@ import { formObj } from "../../helpers/constants";
 // Implement a button that lets all possible combinations be displayed?
 
 export default function Main(props) {
+  const [formElements, setFormElements] = useState(null);
+  const [renderWindowElements, setRenderWindowElements] = useState(null);
+
+  switch (props.tab) {
+    case "badges":
+      console.log("Badges tab selected.");
+      break;
+    case "banners":
+      console.log("Banners tab selected.");
+      break;
+    case "cards":
+      console.log("Cards tab selected.");
+      break;
+    case "testimonials":
+      console.log("Testimonials tab selected.");
+      break;
+    case "tooltips":
+      console.log("Tooltips tab selected.");
+      break;
+    case "toast-popups":
+      console.log("Toast Popups tab selected.");
+      break;
+    default:
+      console.log("Badges tab selected.");
+      break;
+  }
+
   return (
     <main>
       <section className="entry-form">
