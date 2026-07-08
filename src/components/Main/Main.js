a97203337b852bed4375f830203193aa46643156
sjs-code-foundry <104642983+sjs-code-foundry@users.noreply.github.com>
2026-07-08 19:42:50 +0100

    feat: Get tabs working
    Implement state in combination with a switch statement wrapped in a useEffect (to limit re-renders).

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import Form from "../EntryForm";
 import Badge from "../ComponentElements/Badge";
@@ -10,49 +10,55 @@ export default function Main(props) {
   const [formElements, setFormElements] = useState(null);
   const [renderWindowElements, setRenderWindowElements] = useState(null);
 
-  switch (props.tab) {
-    case "badges":
-      console.log("Badges tab selected.");
-      break;
-    case "banners":
-      console.log("Banners tab selected.");
-      break;
-    case "cards":
-      console.log("Cards tab selected.");
-      break;
-    case "testimonials":
-      console.log("Testimonials tab selected.");
-      break;
-    case "tooltips":
-      console.log("Tooltips tab selected.");
-      break;
-    case "toast-popups":
-      console.log("Toast Popups tab selected.");
-      break;
-    default:
-      console.log("Badges tab selected.");
-      break;
-  }
+  useEffect(() => {
+    switch (props.tab) {
+      case "badges":
+        setFormElements(
+          <>
+            <Form.Dropdown
+              labelText="Badge Color: "
+              name="badge-color"
+              options={formObj.badges.colors}
+            />
+            <Form.Radios
+              labelText="Badge Shape: "
+              name="badge-shape"
+              options={formObj.badges.shapes}
+            />
+          </>,
+        );
+        setRenderWindowElements(
+          <Badge text="Badge" style={props.formSubmission} />,
+        );
+        break;
+      case "banners":
+        console.log("Banners tab selected.");
+        break;
+      case "cards":
+        console.log("Cards tab selected.");
+        break;
+      case "testimonials":
+        console.log("Testimonials tab selected.");
+        break;
+      case "tooltips":
+        console.log("Tooltips tab selected.");
+        break;
+      case "toast-popups":
+        console.log("Toast Popups tab selected.");
+        break;
+      default:
+        setFormElements(<p>Form not loaded.</p>);
+        setRenderWindowElements(<p>Rendering Window not loaded.</p>);
+        break;
+    }
+  }, [props.tab, props.formSubmission]);
 
   return (
     <main>
       <section className="entry-form">
-        <Form onSubmission={props.setFormSubmission}>
-          <Form.Dropdown
-            labelText="Badge Color: "
-            name="badge-color"
-            options={formObj.badges.colors}
-          />
-          <Form.Radios
-            labelText="Badge Shape: "
-            name="badge-shape"
-            options={formObj.badges.shapes}
-          />
-        </Form>
+        <Form onSubmission={props.setFormSubmission}>{formElements}</Form>
       </section>
-      <section className="render-window">
-        <Badge text="Badge" style={props.formSubmission} />
-      </section>
+      <section className="render-window">{renderWindowElements}</section>
     </main>
   );
 }
