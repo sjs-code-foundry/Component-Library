03a46a2c28175502fab59cb4974d52fdc52162b2
sjs-code-foundry <104642983+sjs-code-foundry@users.noreply.github.com>
2026-08-03 14:06:16 +0100

    feat: Implement toast popup icon and space & format text

--- src/components/ComponentElements/ToastPopup.js
+++ src/components/ComponentElements/ToastPopup.js
@@ -1,22 +1,40 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, createElement } from "react";
 
 export default function ToastPopup(props) {
-  const [componentCSS, setComponentCSS] = useState({ display: "none" });
+  const [componentCSS, setComponentCSS] = useState({ display: "flex" });
+  const [iconSVG, setIconSVG] = useState(null);
+  const [title, setTitle] = useState("Biscuits!");
+  const [message, setMessage] = useState(
+    "as prose aisle flow made did way to his memory",
+  );
 
   useEffect(() => {
     if (props.properties) {
-      console.log(props.properties.get("toast-popup-type"));
       console.log(props.properties.get("toast-popup-title"));
       console.log(props.properties.get("toast-popup-message"));
 
-      setComponentCSS({ display: "block" });
+      const popupType = JSON.parse(props.properties.get("toast-popup-type"));
+      console.log(popupType);
+
+      setIconSVG(
+        createElement(
+          "svg",
+          popupType.iconProps,
+          createElement("path", popupType.iconChildProps),
+        ),
+      );
+
+      setComponentCSS({ display: "flex" });
     }
   }, [props.properties]);
 
   return (
     <div className="toast-popup" style={componentCSS}>
-      {/* Contents of component here */}
-      <p>This is a toast popup.</p>
+      {iconSVG}
+      <div className="toast-popup-textarea">
+        <h1>{title}</h1>
+        <p>{message}</p>
+      </div>
     </div>
   );
 }
